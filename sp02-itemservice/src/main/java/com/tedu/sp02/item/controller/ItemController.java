22de426f22c727337bce57a23c2dd08ee50999d0
Administrator <Administrator@A7YTIYMNNSPJD0Y>
2019-09-28 15:47:08 +0800

    1

--- sp02-itemservice/src/main/java/com/tedu/sp02/item/controller/ItemController.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/controller/ItemController.java
@@ -1,10 +1,12 @@
 package com.tedu.sp02.item.controller;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,27 +20,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class ItemController {
-
 	@Autowired
 	private ItemService itemService;
 	
-	//为了后面测试集群
 	@Value("${server.port}")
 	private int port;
 	
 	@GetMapping("/{orderId}")
-	public JsonResult<List<Item>> getItems(String orderId) {
-		log.info("server.port= "+port+", orderId= "+orderId);
+	public JsonResult<List<Item>> getItems(@PathVariable String orderId) throws InterruptedException {
+		log.info("server.port="+port+", orderId="+orderId);
+		
+		///--设置随机延迟
+		long t = new Random().nextInt(5000);
+		if(Math.random()<0.6) {
+			log.info("item-service-"+port+" - 暂停"+t);
+			Thread.sleep(t);
+		}
+		///~~
 		
 		List<Item> items = itemService.getItems(orderId);
-		return JsonResult.ok(items);
+		return JsonResult.ok(items).msg("port="+port);
 	}
 	
 	@PostMapping("/decreaseNumber")
-	public JsonResult decreaseNumber(@RequestBody List<Item> list) {
-		log.info("减少商品库存");
-		
-		itemService.decreaseNumbers(list);
+	public JsonResult decreaseNumber(@RequestBody List<Item> items) {
+		itemService.decreaseNumbers(items);
 		return JsonResult.ok();
 	}
 }
--- sp02-itemservice/src/main/java/com/tedu/sp02/item/service/ItemServiceImpl.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/service/ItemServiceImpl.java
@@ -12,27 +12,25 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class ItemServiceImpl implements ItemService{
+public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public List<Item> getItems(String orderId) {
 		ArrayList<Item> list = new ArrayList<Item>();
-		list.add(new Item(1, "aaa", 5));
-		list.add(new Item(2, "bbb", 2));
-		list.add(new Item(3, "ccc", 6));
-		list.add(new Item(4, "ddd", 1));
-		list.add(new Item(5, "eee", 4));
+		list.add(new Item(1, "商品 1",1));
+		list.add(new Item(2, "商品 2",2));
+		list.add(new Item(3, "商品 3",3));
+		list.add(new Item(4, "商品 4",4));
+		list.add(new Item(5, "商品 5",5));
 		return list;
 	}
 
 	@Override
 	public void decreaseNumbers(List<Item> list) {
-		if(log.isInfoEnabled()) {
+		if (log.isInfoEnabled()) {
 			for(Item item : list) {
-				log.info("减少库存: "+item);
+				log.info("减少库存 - "+item);
 			}
 		}
-		
 	}
-
 }
